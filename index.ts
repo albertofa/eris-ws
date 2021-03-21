889ec851924623ee1eb3be734fe0dc099b0862c0
Jose Alberto <jose.alberto@cmtecnologia.com.br>
2021-03-21 03:56:53 -0300

    Updates

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import history from 'connect-history-api-fallback'
 import cors from 'cors'
-import WebSocketServer from './websocket/webSocketServer'
+import io, { Socket } from 'socket.io'
 import fs from 'fs'
+import MessageController from './api/controllers/message'
+import security from './utils/security'
 
 const expressServer = express()
 
@@ -31,7 +33,33 @@ import http from 'http'
 import https from 'https'
 const server = new http.Server(expressServer)
 
-const ws = new WebSocketServer(server)
+const ws = new io.Server(server,  { cors: { origin: '*' } })
+ws.on('connection', (socket: Socket) => {
+    console.log('connected', socket.id)
+    
+    socket.on('newMessage', (data: any) => {
+        const user = security.getUserFromToken(data.token)
+        if (user) {
+            MessageController.createMessage(data.message, user.id, (message: any) => {
+                socket.broadcast.emit('newMessage', message)
+                socket.emit('newMessage', message)
+            })
+        }
+    })
+
+    const connectedUsers: any = []
+    socket.on('joinVoice', (data: string) => {
+        console.log('joinVoice', connectedUsers)
+        socket.join(data)
+        connectedUsers.forEach((room: any) => {
+            if (room != data && room)
+                socket.to(room).emit('callVoice', data)
+        });
+        
+        if (!connectedUsers.includes(data))
+            connectedUsers.push(data)
+    })
+})
 
 server.listen(8085, () => {
     console.log('Listening 8085')
@@ -48,7 +76,33 @@ if (process.env.IS_TLS == 'true'){
         ca: ca
     }
     const serverHttps = new https.Server(credentials, expressServer)
-    const wss = new WebSocketServer(serverHttps)
+    const wss = new io.Server(serverHttps,  { cors: { origin: '*' } })
+    wss.on('connection', (socket: Socket) => {
+        console.log('connected', socket.id)
+        
+        socket.on('newMessage', (data: any) => {
+            const user = security.getUserFromToken(data.token)
+            if (user) {
+                MessageController.createMessage(data.message, user.id, (message: any) => {
+                    socket.broadcast.emit('newMessage', message)
+                    socket.emit('newMessage', message)
+                })
+            }
+        })
+
+        const connectedUsers: any = []
+        socket.on('joinVoice', (data: string) => {
+            console.log('joinVoice', connectedUsers)
+            socket.join(data)
+            connectedUsers.forEach((room: any) => {
+                if (room != data && room)
+                    socket.to(room).emit('callVoice', data)
+            });
+            
+            if (!connectedUsers.includes(data))
+                connectedUsers.push(data)
+        })
+    })
    
     serverHttps.listen(443, () => {
         console.log('Listening 443')
